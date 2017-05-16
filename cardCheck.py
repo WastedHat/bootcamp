aa1157ff3fdcfe15eee02f0139a6cdcb63a71e22
WastedHat <saltwaterhippo@gmail.com>
2017-05-16 15:17:23 +0100

    Added card issuer to cardCheck.py

--- cardCheck.py
+++ cardCheck.py
@@ -3,19 +3,19 @@ from functools import reduce
 def isValid(card_num):    #Use the Luhn algorithm to check if card number is valid
 
     #Store card number as list with list comprehension
-    numList = [num for num in card_num.replace(" ", "")]
+    num_list = [num for num in card_num.replace(" ", "")]
 
     #Starting with the first digit, multiply every second digit by 2
     for i in range(0, 15, 2):
-        numList[i] = int(numList[i]) * 2
+        num_list[i] = int(num_list[i]) * 2
 
         #Everytime you have a two digit number, add those digits together
-        if numList[i] > 10:
-            numList[i] = (numList[i] % 10) + 1
+        if num_list[i] > 10:
+            num_list[i] = (num_list[i] % 10) + 1
 
     #Sum of all numbers
     result = 0
-    for i in numList:
+    for i in num_list:
         result += int(i)
 
     #Check to see if result is multiple of 10
@@ -24,7 +24,19 @@ def isValid(card_num):    #Use the Luhn algorithm to check if card number is val
     else:
         print("Invalid card number")
 
-    print(result)
+
+def issuerID(card_num):     #Find card issuer from Issuer Identification Number
+
+    if int(card_num[:2]) >= 51 and int(card_num[:2]) <= 55:
+        print("Card issuer is MasterCard")
+
+    if int(card_num[0]) == 4:
+        print("Card issuer is Visa")
+
+    if int(card_num[:2]) == 34 or int(card_num[:2]) == 37:
+        print("Card issuer is American Express")
 
 
+#Call functions
 isValid("4578 4230 1376 9219")
+issuerID("4578 4230 1376 9219")
