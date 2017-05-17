d8eb6c8515d7443613f3ab526fc1259603e647f3
WastedHat <saltwaterhippo@gmail.com>
2017-05-17 10:58:57 +0100

    added room info with globals

--- hotel.py
+++ hotel.py
@@ -1,37 +1,66 @@
-import shelve, sys
+
+"""INIT"""
+import shelve, os
 shelf_file = shelve.open("data")
 shelf_file.setdefault("count", 1)
 print(shelf_file["count"])
 
+
+"""GLOBALS"""
+penthouse = {"available": 1, "price": 1000, "adults": 25}
+luxury = {"available": 25, "price":250, "adults":5, "children":5}
+standard_single = {"available": 25, "price": 50, "adults": 1, "children": 0}
+standard_twin_single = {"available": 25, "price": 75, "adults": 2, "children": 2}
+standard_double = {"available": 25, "price": 99, "adults": 2, "children": 0}
+standard_twin_double = {"available": 25, "price": 99, "adults": 2, "children": 2}
+
+
+
+"""CLASSES"""
 class Guest(object):
 
-    def __init__(self, first_name, second_name, adults, children, deposit=200):
+    def __init__(self, first_name, second_name, adults, children):
         self.first_name = first_name
         self.second_name = second_name
         self.adults = adults
         self.children = children
+        self.deposit = 200
+        self.balance = 0
 
-    def room(self):
+    def check_in(self):
         pass
 
-class Rooms(object)
+    def check_out(self):
+        pass
 
 
 
-def interface():
+"""METHODS"""
 
-    print("1: View available rooms")
-    print("2: Check in guests")
-    print("3: View current guests")
-    print("4: Check out guests")
-    print("5: Exit program")
+def interface():    #Display menu
+    os.system("cls")
+    print(" 1 : View available rooms")
+    print(" 2 : Check in guests")
+    print(" 3 : View current guests")
+    print(" 4 : Check out guests")
+    print(" 5 : Exit program")
 
     while True:
-        get_num = input("Enter number: ")
+        get_num = input("Enter number: ") # get menu selection
 
-        if get_num == "1":
+        if get_num == "1":      #Display room availability
+            os.system("cls")
+            print("Available rooms: \n{}\n{}\n{}\n{}\n{}\n{}".format(
+                "Penthouse: " + str(penthouse["available"]),
+                "Luxury: " + str(luxury["available"]),
+                "Standard single: " + str(standard_single["available"]),
+                "Standard twin single: " + str(standard_twin_single["available"]),
+                "Standard double: " + str(standard_double["available"]),
+                "Standard twin double: " + str(standard_twin_double["available"]),
+            ))
+            input("Press any key to continue.. ")
+            interface()
 
-            break
 
         elif get_num == "2":    # Get guest details
             first_name = input("Enter guests first name: ")
@@ -40,7 +69,6 @@ def interface():
             children = input("Enter number of children: ")
 
             #Create new guest, print details and create new class instance
-
             shelf_file["count"] += 1
             guest_id = "guest" + str(shelf_file["count"])
             print(guest_id +
@@ -51,21 +79,24 @@ def interface():
 
             guest_id = Guest(first_name, second_name, adults, children)
 
+            print()
             interface()
             break
 
         elif get_num == "3":
-
+            os.system("cls")
 
             break
 
         elif get_num == "4":
-            #checkOut()
+            os.system("cls")
             break
 
         elif get_num == "5":
             shelf_file.close()
             break
 
+
+"""MAIN"""
 
 interface()
