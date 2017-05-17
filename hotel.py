9a5b57f649dc4959b471cfcf72c2c3b5da6b608f
WastedHat <saltwaterhippo@gmail.com>
2017-05-17 14:22:04 +0100

    changed interface() to menu() and split items into methods. updated input validation

--- hotel.py
+++ hotel.py
@@ -1,12 +1,17 @@
+"""To add:
+# Dates with calendar
+"""
 
 """INIT"""
-import shelve, os
+
+import shelve, os, calendar
 shelf_file = shelve.open("data")
 shelf_file.setdefault("count", 1)
-print(shelf_file["count"])
+guest_data = open("guest_data.txt","a")
 
 
 """GLOBALS"""
+
 penthouse = {"available": 1, "price": 1000, "adults": 25}
 luxury = {"available": 25, "price":250, "adults":5, "children":5}
 standard_single = {"available": 25, "price": 50, "adults": 1, "children": 0}
@@ -15,8 +20,8 @@ standard_double = {"available": 25, "price": 99, "adults": 2, "children": 0}
 standard_twin_double = {"available": 25, "price": 99, "adults": 2, "children": 2}
 
 
-
 """CLASSES"""
+
 class Guest(object):
 
     def __init__(self, first_name, second_name, adults, children):
@@ -28,16 +33,73 @@ class Guest(object):
         self.balance = 0
 
     def check_in(self):
-        pass
+
+        confirm = input("Are you sure you want to check guest ")
 
     def check_out(self):
         pass
 
 
-
 """METHODS"""
 
-def interface():    #Display menu
+def display_available_rooms():
+    os.system("cls")
+
+    print("Available rooms: \n\n{}\n{}\n\n{}\n{}\n\n{}\n{}\n".format(
+        "Penthouse: " + str(penthouse["available"]),
+        "Luxury: " + str(luxury["available"]),
+        "Standard single: " + str(standard_single["available"]),
+        "Standard twin single: " + str(standard_twin_single["available"]),
+        "Standard double: " + str(standard_double["available"]),
+        "Standard twin double: " + str(standard_twin_double["available"]),
+    ))
+    input("Press any key to continue.. ")
+    menu()
+
+
+def get_guest_details(): #with input validation
+    os.system("cls")
+
+    first_name = ""
+    while not first_name.strip().isalpha():
+        first_name = input("Enter first name: ")
+
+    second_name = ""
+    while not second_name.strip().isalpha():
+        second_name = input("Enter second name: ")
+
+    adults = ""
+    while not adults.isdigit():
+        adults = input("Enter number of adults: ")
+
+    children = ""
+    while not children.isdigit():
+        children = input("Enter number of children: ")
+
+    nights = ""
+    while not nights.isdigit():
+        nights = input("Enter number of nights: ")
+
+
+    # Create new guest, print details and create new class instance
+    shelf_file["count"] += 1
+    guest_id = "ID: Guest" + str(shelf_file["count"])
+    print("\n" + guest_id +
+          "\nFirst: " + first_name +
+          "\nSecond: " + second_name +
+          "\nAdults: " + adults +
+          "\nChildren: " + children +
+          "\nNights: " + nights + "\n")
+
+
+    guest_id = Guest(first_name, second_name, adults, children)
+    input("Press any key to continue.. ")
+    menu()
+    
+
+
+
+def menu():    #Display menu
     os.system("cls")
     print(" 1 : View available rooms")
     print(" 2 : Check in guests")
@@ -46,57 +108,28 @@ def interface():    #Display menu
     print(" 5 : Exit program")
 
     while True:
-        get_num = input("Enter number: ") # get menu selection
+        menu_item = input("Enter number: ") 
 
-        if get_num == "1":      #Display room availability
-            os.system("cls")
-            print("Available rooms: \n{}\n{}\n{}\n{}\n{}\n{}".format(
-                "Penthouse: " + str(penthouse["available"]),
-                "Luxury: " + str(luxury["available"]),
-                "Standard single: " + str(standard_single["available"]),
-                "Standard twin single: " + str(standard_twin_single["available"]),
-                "Standard double: " + str(standard_double["available"]),
-                "Standard twin double: " + str(standard_twin_double["available"]),
-            ))
-            input("Press any key to continue.. ")
-            interface()
+        if menu_item == "1":      
+            display_available_rooms()
+            
+        elif menu_item == "2":    
+            get_guest_details()
 
-
-        elif get_num == "2":    # Get guest details
-            first_name = input("Enter guests first name: ")
-            second_name = input("Enter guests second name: ")
-            adults = input("Enter number of adults: ")
-            children = input("Enter number of children: ")
-
-            #Create new guest, print details and create new class instance
-            shelf_file["count"] += 1
-            guest_id = "guest" + str(shelf_file["count"])
-            print(guest_id +
-                   "\nFirst name: " + first_name +
-                   "\nSecond name: " + second_name +
-                   "\nAdults: " + adults +
-                   "\nChildren: " + children + "\n")
-
-            guest_id = Guest(first_name, second_name, adults, children)
-
-            print()
-            interface()
-            break
-
-        elif get_num == "3":
+        elif menu_item == "3":
             os.system("cls")
 
             break
 
-        elif get_num == "4":
+        elif menu_item == "4":
             os.system("cls")
             break
 
-        elif get_num == "5":
+        elif menu_item == "5":
             shelf_file.close()
             break
 
 
 """MAIN"""
 
-interface()
+menu()
