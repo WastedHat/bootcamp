cbf858bb6a12f9abe87343cbd34095fe28161ec2
WastedHat <saltwaterhippo@gmail.com>
2017-05-18 14:48:20 +0100

    first major changes since removing classes - changes global dict structure, started view_guest_details method, write guest_data to shelve file, added check_room_availability but contains bug - to finish

--- hotel/hotel.py
+++ hotel/hotel.py
@@ -5,19 +5,19 @@
 """INIT"""
 
 import shelve, os, calendar, time
-shelf_file = shelve.open("var_data")
-shelf_file.setdefault("count", 1)
+guest_data = shelve.open("var_data")
+guest_data.setdefault("count", 10000)
 
 
 """GLOBALS"""
-
-penthouse = {"available": 1, "price": 1000, "adults": 25}
-luxury = {"available": 5, "price":250, "adults":5, "children":5}
-standard_single = {"available": 5, "price": 50, "adults": 1, "children": 0}
-twin_single = {"available": 5, "price": 75, "adults": 2, "children": 2}
-standard_double = {"available": 5, "price": 99, "adults": 2, "children": 0}
-twin_double = {"available": 5, "price": 99, "adults": 2, "children": 2}
-
+room_data = {
+        "P": {"type": "Penthouse", "available": 1, "price": 1000, "adults": 25},
+        "L": {"type": "Luxury", "available": 5, "price": 250, "adults": 5, "children":5},
+        "SS": {"type": "Standard Single", "available": 5, "price": 50, "adults": 1, "children": 0},
+        "TS": {"type": "Twin Single", "available": 5, "price": 75, "adults": 2, "children": 2},
+        "SD": {"type": "Standard Double", "available": 5, "price": 99, "adults": 2, "children": 0},
+        "TD": {"type": "Twin Double", "available": 5, "price": 99, "adults": 2, "children": 2}
+        }
 
 
 """METHODS"""
@@ -26,19 +26,28 @@ def display_available_rooms(called_from=""):
     os.system("cls")
 
     print("Available rooms: \n{}\n{}\n{}\n{}\n{}\n{}\n".format(
-        "[P] Penthouse: " + str(penthouse["available"]),
-        "[L] Luxury: " + str(luxury["available"]),
-        "[SS] Standard single: " + str(standard_single["available"]),
-        "[TS] Twin single: " + str(twin_single["available"]),
-        "[SD] Standard double: " + str(standard_double["available"]),
-        "[TD] Twin double: " + str(twin_double["available"]),
+        "[P]  Penthouse: " + str(room_data["P"]["available"]),
+        "[L]  Luxury: " + str(room_data["L"]["available"]),
+        "[SS] Standard single: " + str(room_data["SS"]["available"]),
+        "[TS] Twin single: " + str(room_data["TS"]["available"]),
+        "[SD] Standard double: " + str(room_data["SD"]["available"]),
+        "[TD] Twin double: " + str(room_data["TD"]["available"]),
     ))
-    input("Press any key to continue.. ")
 
     if called_from == "menu":
+        input("Press any key to return to menu..")
         menu()
     else:
         return
+
+
+def check_room_availability(room): # extend to take in date and nights
+
+    if room_data[room]["available"] <= 0:
+        print("No " + room_data[room]["type"] + " rooms available.")
+        return False
+    else:
+        room_data[room]["available"] -= 1
 
 
 def get_guest_details(): #with input validation
@@ -60,50 +69,70 @@ def get_guest_details(): #with input validation
     while not children.isdigit():
         children = input("Enter number of children: ")
 
+    check_in_date = ""
+    while not check_in_date:
+        check_in_date = input("Enter check in date (DD/MM/YY): ")
+
     nights = ""
     while not nights.isdigit():
         nights = input("Enter number of nights: ")
 
+    room = ""
+    display_available_rooms()
+    while room not in ["P", "L", "SS", "TS", "SD", "TD"]: # TO FIX - ADD ROOM AVAILABILITY CHECK
+            room = input("Enter room: ").upper()
 
-    # Create new guest, print details and create new class instance
-    shelf_file["count"] += 1
-    guest_id = "ID: Guest" + str(shelf_file["count"])
 
+    store_guest_to_database(first_name, second_name, adults, children, check_in_date, nights, room)
+
+
+def store_guest_to_database(first_name, second_name, adults, children, check_in_date, nights, room):
+
+    # create guest ID
+    os.system("cls")
+    guest_data["count"] += 1
+    guest_id = "GuestID: " + str(guest_data["count"])[1:]
+
+    # display input data before confirmation
     print("\n" + guest_id +
           "\nFirst: " + first_name +
           "\nSecond: " + second_name +
           "\nAdults: " + adults +
           "\nChildren: " + children +
-          "\nNights: " + nights + "\n")
+          "\nCheck in: " + check_in_date +
+          "\nNights: " + nights +
+          "\nRoom: " + room_data[room]["type"])
 
     confirm = ""
-    while confirm not in ["Y","N"]:
-        confirm = input("Please confirm guest details are correct Y/N: ")
+    while confirm not in ["Y", "N"]:
+        confirm = input("\nPlease confirm guest details are correct Y/N: ")
+
         if confirm.upper() == "Y":
-            guest_data = open("guest_data.txt", "a")
-            guest_data.write(guest_id + ", " + first_name + ", " + second_name + ", "
-                             + adults + ", " + children + ", " + nights + "\n")
-            guest_data.close()
-            guest_id = Guest(first_name, second_name, adults, children, nights)
-
-            display_available_rooms()
-            guest_id.check_in()
-
-
+            guest_data[guest_id] = {"first_name": first_name, "second_name": second_name,
+                                    "adults": adults, "children": children, "nights": nights,
+                                    "room": room_data[room]["type"]}
+            print(guest_id + " " + first_name + " " + second_name + " saved to database.")
+            input()
+            menu()
 
         elif confirm.upper() == "N":
             print("Cancelled.. exiting to main menu.. ")
             time.sleep(2)
             menu()
-    
 
+
+def view_guest_details():
+
+    for guest_id in guest_data.keys():
+        print(guest_id)
 
 
 def menu():    #Display menu
     os.system("cls")
+    #print(calendar.calendar(17))
     print(" 1 : View available rooms")
     print(" 2 : Check in guests")
-    print(" 3 : View current guests")
+    print(" 3 : View guest details")
     print(" 4 : Check out guests")
     print(" 5 : Exit program")
 
@@ -117,19 +146,17 @@ def menu():    #Display menu
             get_guest_details()
 
         elif menu_item == "3":
-            os.system("cls")
-            break
+            view_guest_details()
 
         elif menu_item == "4":
             os.system("cls")
             break
 
         elif menu_item == "5":
-            shelf_file.close()
+            guest_data.close()
 
             break
 
 
 """MAIN"""
-
 menu()
