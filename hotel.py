7c5548632b3334e8244bdecd25846a3183887851
WastedHat <saltwaterhippo@gmail.com>
2017-05-18 10:01:03 +0100

    last commit before first branch - contains basic class Guest

--- hotel.py
+++ hotel.py
@@ -4,37 +4,38 @@
 
 """INIT"""
 
-import shelve, os, calendar
+import shelve, os, calendar, time
 shelf_file = shelve.open("data")
 shelf_file.setdefault("count", 1)
-guest_data = open("guest_data.txt","a")
 
 
 """GLOBALS"""
 
 penthouse = {"available": 1, "price": 1000, "adults": 25}
-luxury = {"available": 25, "price":250, "adults":5, "children":5}
-standard_single = {"available": 25, "price": 50, "adults": 1, "children": 0}
-standard_twin_single = {"available": 25, "price": 75, "adults": 2, "children": 2}
-standard_double = {"available": 25, "price": 99, "adults": 2, "children": 0}
-standard_twin_double = {"available": 25, "price": 99, "adults": 2, "children": 2}
+luxury = {"available": 5, "price":250, "adults":5, "children":5}
+standard_single = {"available": 5, "price": 50, "adults": 1, "children": 0}
+twin_single = {"available": 5, "price": 75, "adults": 2, "children": 2}
+standard_double = {"available": 5, "price": 99, "adults": 2, "children": 0}
+twin_double = {"available": 5, "price": 99, "adults": 2, "children": 2}
 
 
 """CLASSES"""
 
 class Guest(object):
 
-    def __init__(self, first_name, second_name, adults, children):
+    def __init__(self, first_name, second_name, adults, children, nights):
         self.first_name = first_name
         self.second_name = second_name
         self.adults = adults
         self.children = children
+        self.nights = nights
         self.deposit = 200
         self.balance = 0
 
-    def check_in(self):
+    def check_in(self): # set dates
 
-        confirm = input("Are you sure you want to check guest ")
+        check_in_date = input("Enter check in date (DD/MM/YYYY): ")
+
 
     def check_out(self):
         pass
@@ -42,19 +43,23 @@ class Guest(object):
 
 """METHODS"""
 
-def display_available_rooms():
+def display_available_rooms(called_from=""):
     os.system("cls")
 
-    print("Available rooms: \n\n{}\n{}\n\n{}\n{}\n\n{}\n{}\n".format(
-        "Penthouse: " + str(penthouse["available"]),
-        "Luxury: " + str(luxury["available"]),
-        "Standard single: " + str(standard_single["available"]),
-        "Standard twin single: " + str(standard_twin_single["available"]),
-        "Standard double: " + str(standard_double["available"]),
-        "Standard twin double: " + str(standard_twin_double["available"]),
+    print("Available rooms: \n{}\n{}\n{}\n{}\n{}\n{}\n".format(
+        "[P] Penthouse: " + str(penthouse["available"]),
+        "[L] Luxury: " + str(luxury["available"]),
+        "[SS] Standard single: " + str(standard_single["available"]),
+        "[TS] Twin single: " + str(twin_single["available"]),
+        "[SD] Standard double: " + str(standard_double["available"]),
+        "[TD] Twin double: " + str(twin_double["available"]),
     ))
     input("Press any key to continue.. ")
-    menu()
+
+    if called_from == "menu":
+        menu()
+    else:
+        return
 
 
 def get_guest_details(): #with input validation
@@ -84,6 +89,7 @@ def get_guest_details(): #with input validation
     # Create new guest, print details and create new class instance
     shelf_file["count"] += 1
     guest_id = "ID: Guest" + str(shelf_file["count"])
+
     print("\n" + guest_id +
           "\nFirst: " + first_name +
           "\nSecond: " + second_name +
@@ -91,10 +97,25 @@ def get_guest_details(): #with input validation
           "\nChildren: " + children +
           "\nNights: " + nights + "\n")
 
+    confirm = ""
+    while confirm not in ["Y","N"]:
+        confirm = input("Please confirm guest details are correct Y/N: ")
+        if confirm.upper() == "Y":
+            guest_data = open("guest_data.txt", "a")
+            guest_data.write(guest_id + ", " + first_name + ", " + second_name + ", "
+                             + adults + ", " + children + ", " + nights + "\n")
+            guest_data.close()
+            guest_id = Guest(first_name, second_name, adults, children, nights)
 
-    guest_id = Guest(first_name, second_name, adults, children)
-    input("Press any key to continue.. ")
-    menu()
+            display_available_rooms()
+            guest_id.check_in()
+
+
+
+        elif confirm.upper() == "N":
+            print("Cancelled.. exiting to main menu.. ")
+            time.sleep(2)
+            menu()
     
 
 
@@ -111,14 +132,13 @@ def menu():    #Display menu
         menu_item = input("Enter number: ") 
 
         if menu_item == "1":      
-            display_available_rooms()
+            display_available_rooms("menu")
             
         elif menu_item == "2":    
             get_guest_details()
 
         elif menu_item == "3":
             os.system("cls")
-
             break
 
         elif menu_item == "4":
@@ -127,6 +147,7 @@ def menu():    #Display menu
 
         elif menu_item == "5":
             shelf_file.close()
+
             break
 
 
